aa41ff37dba9806d834529391d97ccf6efac97e4
Sebastian Vasquez <sebastian.vqz@gmail.com>
2016-01-05 11:42:41 -0300

    test 2
    se añadio una cosa

--- app/views/layouts/master.blade.php
+++ app/views/layouts/master.blade.php
@@ -15,7 +15,8 @@
       <li class="active"><a href="#">Home</a></li>
       <li><a href="#">Page 1</a></li>
       <li><a href="#">Page 2</a></li> 
-      <li><a href="#">Page 3</a></li> 
+      <li><a href="#">Page 3</a></li>
+      <li><a href="#">Page 4</a></li> 
     </ul>
   </div>
 </nav>
