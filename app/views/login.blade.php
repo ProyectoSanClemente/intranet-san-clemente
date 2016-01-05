455342a6e16a9b0a954979785976e44faaf0c082
Sebastian Vasquez <sebastian.vqz@gmail.com>
2016-01-05 12:36:09 -0300

    login
    falta final de login

--- app/views/login.blade.php
+++ app/views/login.blade.php
@@ -1,35 +1,29 @@
-<h2>Ingresar</h2>
-
-@if (Session::has('mensaje_login'))
-<span>{{ Session::get('mensaje_login') }}</span>
-@endif
-
-{{ Form::open(array('url' => 'login','class' => 'btn btn-primary btn-lg" href="#" role="button')) }}
-
-    {{ Form::label('correo', 'Correo'); }}
-    {{ Form::text('correo','',array('class' => 'btn btn-primary btn-lg" href="#" role="button')); }}
-    {{ Form::label('password', 'Clave'); }}
-    {{ Form::password('password'); }}
-    {{ Form::submit('Ingresar'); }}
-
-{{ Form::close() }}
+<html>
+<head>
+{{ HTML::style('bootstrap/css/bootstrap.css')}}
+{{ HTML::style('bootstrap/css/bootstrap-theme.css')}}
+{{ HTML::style('bootstrap/css/bootstrap.min.js')}}
 
 
+</head> 
+    <body>
 
-    <div class="container">
+        <div class="container">
+            		<h2>Ingresar</h2>
 
-      <form class="form-signin">
-        <h2 class="form-signin-heading">Please sign in</h2>
-        <label for="inputEmail" class="sr-only">Email address</label>
-        <input type="email" id="inputEmail" class="form-control" placeholder="Email address" required autofocus>
-        <label for="inputPassword" class="sr-only">Password</label>
-        <input type="password" id="inputPassword" class="form-control" placeholder="Password" required>
-        <div class="checkbox">
-          <label>
-            <input type="checkbox" value="remember-me"> Remember me
-          </label>
+		@if (Session::has('mensaje_login'))
+		<span>{{ Session::get('mensaje_login') }}</span>
+		@endif
+
+		{{ Form::open(array('url' => 'login','class' => 'form-signin')) }}
+		    {{ Form::label('correo', 'Correo',array('class' => 'sr-only')); }}
+		    {{ Form::text('correo','',array('class' => 'btn btn-primary btn-lg','placeholder' => 'Direccion de Correo', 'required autofocus')); }}
+		    {{ Form::label('password', 'Clave',array('class' => 'sr-only')); }}
+		    {{ Form::password('password',array('placeholder' => 'Password', 'required')); }}
+		    {{ Form::submit('Ingresar',array('class' => 'btn btn-lg btn-primary btn-block')); }}
+
+		{{ Form::close() }}
         </div>
-        <button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
-      </form>
 
-    </div> <!-- /container -->
+    </body>
+</html>
