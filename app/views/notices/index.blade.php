2f00af6f918e494c402e73501a5d72dfad07623c
Sebastian Vasquez <sebastian.vqz@gmail.com>
2016-01-06 11:47:42 -0300

    comit2
    comit2

--- app/views/notices/index.blade.php
+++ app/views/notices/index.blade.php
@@ -7,75 +7,43 @@
 
 @section('content')
 
-    <div class="marketing">
-<nav class="navbar navbar-inverse">
-    <div class="navbar-header">
-        <a class="navbar-brand" href="{{ URL::to('notice') }}">Notice Alert</a>
-    </div>
-    <ul class="nav navbar-nav">
-        <li><a href="{{ URL::to('notices') }}">View All Notices</a></li>
-        <li><a href="{{ URL::to('notices/create') }}">Create a Notices</a>
-    </ul>
-</nav>
-  @if (Session::has('message'))
-    <div class="alert alert-info">{{ Session::get('message') }}</div>
-@endif
+    @if (Session::has('mensaje'))
+        <div class="alert alert-info">{{ Session::get('mensaje') }}</div>
+    @endif
 
 <table class="table table-striped table-bordered">
     <thead>
         <tr>
-            <td>ID</td>
-            <td>Name</td>
-            <td>Email</td>
-            <td>Nerd Level</td>
-            <td>Actions</td>
+            <td>Fecha</td>
+            <td>Titulo</td>
+            <td>Contenido</td>
+            <td>Imagen</td>
         </tr>
     </thead>
     <tbody>
-    @foreach($nerds as $key => $value)
+   
+    @foreach($notices as $notice)
         <tr>
-            <td>{{ $value->id }}</td>
-            <td>{{ $value->name }}</td>
-            <td>{{ $value->email }}</td>
-            <td>{{ $value->nerd_level }}</td>
+            <td>{{ $notice->titulo }}
+            <td>{{ $notice->contenido }}</td>
+            <td>{{ $notice->imagen }}</td>
+            <td>{{ $notice->fecha }}</td>
 
             <!-- we will also add show, edit, and delete buttons -->
             <td>
-
                 <!-- delete the nerd (uses the destroy method DESTROY /nerds/{id} -->
                 <!-- we will add this later since its a little more complicated than the other two buttons -->
 
                 <!-- show the nerd (uses the show method found at GET /nerds/{id} -->
-                <a class="btn btn-small btn-success" href="{{ URL::to('notices/' . $value->id) }}">Show this Nerd</a>
+                <a class="btn btn-small btn-success" href="{{ URL::to('notices/' . $notice->id) }}">Mostrar Noticia</a>
 
                 <!-- edit this nerd (uses the edit method found at GET /nerds/{id}/edit -->
-                <a class="btn btn-small btn-info" href="{{ URL::to('notices/' . $value->id . '/edit') }}">Edit this Nerd</a>
+                <a class="btn btn-small btn-info" href="{{ URL::to('notices/' . $notice->id . '/edit') }}">Editar Noticia</a>
 
             </td>
         </tr>
     @endforeach
     </tbody>
 </table>
-      <!-- Three columns of text below the carousel -->
-      <div class="row">
-        <div class="col-lg-4">
-          <img class="img-circle" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Generic placeholder image" width="140" height="140">
-          <h2>Heading</h2>
-          <p>Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula ut id elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Praesent commodo cursus magna.</p>
-          <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
-        </div><!-- /.col-lg-4 -->
-        <div class="col-lg-4">
-          <img class="img-circle" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Generic placeholder image" width="140" height="140">
-          <h2>Heading</h2>
-          <p>Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Cras mattis consectetur purus sit amet fermentum. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh.</p>
-          <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
-        </div><!-- /.col-lg-4 -->
-        <div class="col-lg-4">
-          <img class="img-circle" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Generic placeholder image" width="140" height="140">
-          <h2>Heading</h2>
-          <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
-          <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
-        </div><!-- /.col-lg-4 -->
-      </div><!-- /.row -->
 
 @stop
